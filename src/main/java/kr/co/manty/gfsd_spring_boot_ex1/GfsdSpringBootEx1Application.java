5e7419c8dad013de55ff65fc7c093bc0131c45fa
Manty <jibum.jung@nhndooray.com>
2024-01-01 14:55:51 +0900

    first commit

---
+++ src/main/java/kr/co/manty/gfsd_spring_boot_ex1/GfsdSpringBootEx1Application.java
@@ -0,0 +1,13 @@
+package kr.co.manty.gfsd_spring_boot_ex1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GfsdSpringBootEx1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GfsdSpringBootEx1Application.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/manty/gfsd_spring_boot_ex1/GfsdSpringBootEx1ApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.manty.gfsd_spring_boot_ex1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GfsdSpringBootEx1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
